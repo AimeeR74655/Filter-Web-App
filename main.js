5bd3315d0052c3a33a2f8b52b9bc29c0980ecfc8
AimeeR74655 <82007449+AimeeR74655@users.noreply.github.com>
2022-03-06 19:23:38 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+noseX=0;
+noseY=0;
+
 function preload()
 {
-
+    lipStick= loadImage('https://i.postimg.cc/XNKfGh2s/574-5744218-red-lipstick-transparent-image-red-lips-png-png-removebg-preview.png')
 }
 
 function setup()
@@ -17,7 +20,9 @@ function setup()
 function draw()
 {
     image(video,0,0,300,300);
-  
+    fill(255,0,0);
+    stroke(255,0,0);
+   image(clownNose,noseX,noseY,30,30);
 }
 function take_snapshot()
  {
@@ -36,6 +41,8 @@ function modelLoaded()
          console.log(results);
          noseX = results[0].pose.nose.x;
          noseY = results[0].pose.nose.y;
+         console.log("nose x = " + results[0].pose.nose.x);
+         console.log("nose y = " + results[0].pose.nose.y);
      }
  }
     
